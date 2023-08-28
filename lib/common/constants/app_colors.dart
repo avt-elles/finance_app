3718bb28c02b1bdf15418cdea313b747db8620be
Alexsander Telles <alevtelles@gmail.com>
2023-08-27 21:01:59 -0300

    Criando tema de cores e fonte

---
+++ lib/common/constants/app_colors.dart
@@ -0,0 +1,9 @@
+import 'dart:ui';
+
+class AppColors {
+  AppColors._();
+
+  static const Color greenlightOne = Color(0xff63B5AF);
+  static const Color greenlightTwo = Color(0xFF438883);
+  static const Color withe = Color(0xFFFFFFFF);
+}
